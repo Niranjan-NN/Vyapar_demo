b80f8ce4932f3f58684b31986d09d3bbaa830161
Niranjan-NN <129034340+Niranjan-NN@users.noreply.github.com>
2025-05-18 15:10:59 +0530

    Update vite.config.js

--- vyapar/vite.config.js
+++ vyapar/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
+// https://vite.dev/config/
 export default defineConfig({
-  server: {
-    host: '0.0.0.0', // 👈 this line tells Vite to listen on all interfaces
-  }
-});
+  plugins: [react()],
+})
