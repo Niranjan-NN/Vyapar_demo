08e16ab9ef2ad727f355c263d05eb35f4e7be2b1
Niranjan-NN <129034340+Niranjan-NN@users.noreply.github.com>
2025-05-18 16:00:20 +0530

    Update vite.config.js

--- vyapar/vite.config.js
+++ vyapar/vite.config.js
@@ -1,10 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: process.env.PORT || 5173
+  preview: {
+    allowedHosts: ['vyapar-demo-frontend.onrender.com']
   }
-})
+});
